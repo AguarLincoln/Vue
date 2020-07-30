ba18a7a3fbed48de66fe79b866139be382f0490c
Lincoln Aguiar <lincolnalca@gmail.com>
2020-07-30 08:25:53 -0300

    template login

---
+++ src/pages/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <span>
+    <login-template>
+      <span slot="menu-esquerdo">
+        <img src="@/assets/logo.png" class="responsive-image">
+      </span>
+      
+      <span slot="principal">
+        <h2>Login</h2>
+        <form class="login-form">
+        
+          <grid-vue class="input-field" tamanho= "11">
+            <i class="material-icons prefix">mail_outline</i>
+            <input class="validate" id="email" type="email">
+            <label for="email" data-error="wrong" data-success="right">Email</label>
+          </grid-vue>
+        
+        
+          <grid-vue class="input-field" tamanho= "11">
+            <i class="material-icons prefix">lock_outline</i>
+            <input id="password" type="password">
+            <label for="password">Password</label>
+          </grid-vue>
+    
+          <grid-vue class="input-field" tamanho= "5">
+            <a href="#" class="btn waves-effect waves-light">Login</a>
+          </grid-vue>
+
+          <grid-vue class="input-field"  tamanho= "6">
+            <label>
+              <input type="checkbox" />
+              <span>Remember me</span>
+            </label>
+          </grid-vue>
+          <div class="row"></div>
+          <div class="row">
+            <grid-vue class="input-field" tamanho= "5">
+              <p class="margin medium-small"><a href="#">Register Now!</a></p>
+            </grid-vue>
+            <grid-vue class="input-field" tamanho= "">
+                <p class="margin right-align medium-small"><a href="#">Forgot password?</a></p>
+            </grid-vue>          
+          </div>
+
+        </form>
+      </span>
+    </login-template>
+  </span>
+</template>
+
+<script>
+import GridVue from '@/components/layouts/GridVue'
+import LoginTemplate from '@/templates/LoginTemplate'
+import CardConteudoVue from '@/components/social/CardConteudoVue'
+import ConteudoPost from '@/components/social/ConteudoPost'
+import PublicarConteudo from '@/components/social/PublicarConteudo'
+
+export default {
+  name: 'Login',
+  data () {
+      return{
+
+      }
+  },
+  components:{
+    CardConteudoVue,
+    ConteudoPost,
+    PublicarConteudo,
+    GridVue,
+    LoginTemplate,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/templates/LoginTemplate.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <header>
+      <nav-bar cor="green" logo="LOGO"/>
+    </header>
+  
+  
+    <main>
+      <div class="container">
+        <div class="row">
+          <grid-vue tamanho="6">
+            
+            <slot name="menu-esquerdo"/>
+            
+            
+          </grid-vue>
+          
+          <grid-vue tamanho="6">
+            <slot name="principal"/>
+          </grid-vue>
+        
+        </div>
+      </div>
+      
+    </main>
+
+    
+    <footer-vue cor="green" logo="./assets/logo.png" ano="2020" descricao="Description here!">
+      <li><a class="grey-text text-lighten-3" href="#!">Home</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Perfil</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Page 1</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Page2</a></li>
+    </footer-vue>
+    
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/layouts/NavBar'
+import GridVue from '@/components/layouts/GridVue'
+import CardMenuVue from '@/components/layouts/CardMenuVue'
+import FooterVue from '@/components/layouts/FooterVue'
+export default {
+  name: 'LoginTemplate',
+  components:{
+    NavBar,
+    FooterVue,
+    GridVue,
+    CardMenuVue
+  }
+}
+</script>
+
+<style>
+
+</style>
