7eef2c8491df41af86b92bbe5c39793783a8951f
Lincoln Aguiar <lincolnalca@gmail.com>
2020-08-18 16:58:42 -0300

    adding Vuex

--- src/main.js
+++ src/main.js
@@ -2,15 +2,40 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex'
 import axios from 'axios';
 import router from './router'
+import VueAxios from 'vue-axios';
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 Vue.prototype.$url = 'http://127.0.0.1:8000/api/'
+
+var store = new Vuex.Store({
+  state:{
+    nome: 'Lincoln',
+    sobrenome: 'Aguiar'    
+  },
+  getters: {
+    getNome: state => {
+      return state.nome;
+    },
+    getNomeCompleto: state => {
+      return state.nome + ' ' + state.sobrenome
+    }
+  },
+  mutations:{
+    setNome(state, nome){
+      state.nome = nome;
+    }
+  }
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
